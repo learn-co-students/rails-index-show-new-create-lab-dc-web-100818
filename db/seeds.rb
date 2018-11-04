217fe2fd0541a798073095ba76501af8b73d65de
jennyjean <jennyjean8675309@yahoo.com>
2018-11-03 23:51:17 -0400

    Create a web app using Rails with routes for index, show, new and create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+sushi_go = Coupon.create(coupon_code: "Go Go to Sushi Go!", store: "Sushi Go")
+book = Coupon.create(coupon_code: "Everyone needs a good book to read", store: "Barnes & Noble")
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
